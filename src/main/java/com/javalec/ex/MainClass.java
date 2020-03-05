5b0c61cf5e6982d298e9a84b18804c582fbe617e
이승휘 <looklsh@hanmail.net>
2020-03-05 15:08:04 +0900

    springdicp

---
+++ src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,45 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String configLocation1 = "classpath:applicationCTX.xml";
+		String configLocation2 = "classpath:applicationCTX1.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation1,
+				configLocation2);
+		Student student1 = ctx.getBean("student1", Student.class);
+		System.out.println(student1.getName()); //휘
+		System.out.println(student1.getHobbys()); // 수영, 영화
+				
+		StudentInfo studentInfo = ctx.getBean("studentInfo1", StudentInfo.class);
+		Student student2 = studentInfo.getStudent();
+		System.out.println(student2.getName()); //휘
+		System.out.println(student2.getHobbys()); // 수영, 영화
+		
+		if (student1.equals(student2)) {
+			System.out.println("student1 == student2");
+		}
+		
+		Student student3 = ctx.getBean("student3", Student.class);
+		System.out.println(student3.getName());
+		
+		if (student1.equals(student3)) {
+			System.out.println("student1 == student3");
+		}else {
+			System.out.println("student1 != student3");
+		}
+		
+		Family family = ctx.getBean("family", Family.class);
+		System.out.println(family.getPapaName());
+		System.out.println(family.getBrotherName());
+		
+		ctx.close();
+		
+	}
+
+}
